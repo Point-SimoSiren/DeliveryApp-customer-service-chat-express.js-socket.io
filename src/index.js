2fc873fb64133404cf7dbca4f991bbc82300fc15
Simo Siren <sirensimo7@gmail.com>
2021-12-05 23:16:53 +0200

    changed port definition to 3200

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
-const port = process.env.PORT || 3100
+const port = process.env.PORT || 3200
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
